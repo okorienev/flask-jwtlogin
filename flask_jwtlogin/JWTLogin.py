f8d9189c6012a97cf12731dfb4f0fb389f5e3deb
Oleksandr Korienev <alexkorienev@gmail.com>
2018-06-09 13:06:13 +0300

    generating jwt token,  decorator now works only inside app context

--- flask_jwtlogin/JWTLogin.py
+++ flask_jwtlogin/JWTLogin.py
@@ -1,3 +1,9 @@
+from flask import request
+from flask_jwtlogin.errors import UserLoaderNotSetException
+from flask_jwtlogin import AnonymousUser
+import jwt
+
+
 class JWTLogin:
     instance = None
 
@@ -16,6 +22,27 @@ class JWTLogin:
         def user_loader(self, callback_func):
             """set callback for loading user from your storage"""
             self._callback = callback_func
+
+        # def load_user(self, identifier: str):
+        #     with self.current_app.app_context:
+        #         if not self._callback:
+        #             raise UserLoaderNotSetException("User loader callback was not set")
+        #         token = request.headers.get(self.config.get("JWT_HEADER_NAME"))
+        #         if not token:
+        #             return AnonymousUser
+        #         try:
+                    
+        def generate_jwt_token(self, identifier: str) -> dict:
+            """
+            generating jwt token, works only inside application context
+            :param identifier: string representing unique identifier of your user 
+            :return: dict with structure {"JWT_HEADER_NAME": "your_jwt_token"}  
+            """
+            with self.current_app.app_context:
+                token = jwt.encode({"id": identifier.encode()},
+                                   self.config.get("JWT_SECRET_KEY"),
+                                   algorithm=self.config.get("JWT_ENCODING_ALGORITHM"))
+                return {self.config.get('JWT_HEADER_NAME'): token}
 
     def __init__(self):
         if not JWTLogin.instance:
--- flask_jwtlogin/decorators.py
+++ flask_jwtlogin/decorators.py
@@ -6,15 +6,18 @@ from flask import request, abort
 def jwt_required(func):
     """ 
     checks your request headers to contain jwt with specified name
-    aborts with 401 Unauthorized if was not found
+    aborts with 401 Unauthorized if token was not found
     :param func: view to decorate   
     """
     @wraps(func)
     def decorated_view(*args, **kwargs):
-        if not request.headers.get(JWTLogin().config.get('JWT_HEADER_NAME')):
-            abort(401)
+        with JWTLogin.instance.current_app.app_context():  # to work only inside application context
+            if not request.headers.get(JWTLogin().config.get('JWT_HEADER_NAME')):
+                abort(401)
         return func(*args, **kwargs)
     return decorated_view
+
+
 
 
 
